48116cf16c26a0889dbbeebfd745781e048e844e
peterszerzo <szerzo.peter@gmail.com>
2017-02-21 17:42:07 +0100

    JS standard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-var Elm = require('./Main.elm');
-var db = require('./db');
+var Elm = require('./Main.elm')
+var db = require('./db')
 
-var app = Elm.Main.embed(document.getElementById('root'));
+var app = Elm.Main.embed(document.getElementById('root'))
 
-function talkToGame(ports) {
-  ports.connectToRoom.subscribe(function(roomId) {
-    db.subscribeToRoom(roomId, function(room) {
-      ports.roomUpdated.send(JSON.stringify(room));
-    });
-  });
-  ports.createRoom.subscribe(function(room) {
-    db.updateRoom(JSON.parse(room));
-  });
+function talkToGame (ports) {
+  ports.connectToRoom.subscribe(function (roomId) {
+    db.subscribeToRoom(roomId, function (room) {
+      ports.roomUpdated.send(JSON.stringify(room))
+    })
+  })
+  ports.createRoom.subscribe(function (room) {
+    db.updateRoom(JSON.parse(room))
+  })
 }
 
-talkToGame(app.ports);
+talkToGame(app.ports)
