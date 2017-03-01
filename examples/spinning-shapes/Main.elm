4bf0759575ae90aef5e47a6aa34d23c3746db696
peterszerzo <szerzo.peter@gmail.com>
2017-03-01 08:58:11 +0100

    Finish spinning shapes example

--- examples/spinning-shapes/Main.elm
+++ examples/spinning-shapes/Main.elm
@@ -4,7 +4,7 @@ import Random
 import Html exposing (Html, div, text, span)
 import Html.Attributes exposing (class, style, attribute)
 import Html.Events exposing (onClick)
-import Svg exposing (polygon, svg)
+import Svg exposing (polygon, svg, g)
 import Svg.Attributes exposing (width, height, viewBox, points, transform)
 import Json.Encode as JE
 import Json.Decode as JD
@@ -30,10 +30,11 @@ gameSpec =
     { view =
         (\playerId players problem ->
             div
-                [ style
+                [ class "spinning-shapes-container"
+                , style
                     [ ( "position", "absolute" )
-                    , ( "width", "30vmin" )
-                    , ( "height", "30vmin" )
+                    , ( "width", "80vmin" )
+                    , ( "height", "80vmin" )
                     , ( "top", "50%" )
                     , ( "left", "50%" )
                     , ( "transform", "scale(1.0, 1.0) translate3d(-50%, -50%, 0)" )
@@ -44,9 +45,11 @@ gameSpec =
                         (\index { x, y } ->
                             let
                                 translateString =
-                                    ("translate(" ++ (toString (x * 900)) ++ "," ++ (toString (y * 900)) ++ ")")
+                                    ("translate(" ++ (toString (x * 900 + 50)) ++ "," ++ (toString (y * 900 + 50)) ++ ")")
                             in
-                                polygon [ points "0,0 100,0 50,86.6", transform translateString, attribute "data-translate" translateString, onClick index ] []
+                                g [ transform translateString ]
+                                    [ polygon [ points "-50,-28.8 50,-28.8 0,57.7", onClick index ] []
+                                    ]
                         )
                         problem
                     )
