09b7d5f99be9d2769641a9418f2e65f9c44ac41b
peterszerzo <szerzo.peter@gmail.com>
2017-04-30 08:51:58 +0200

    Split up Update module

--- src/Update.elm
+++ src/Update.elm
@@ -1,8 +1,6 @@
 module Update exposing (..)
 
 import Navigation
-import Random
-import Dict
 import Commands as Commands
 import Messages exposing (..)
 import Models.Room as Room
@@ -11,10 +9,9 @@ import Gameroom.Spec exposing (Spec)
 import Models.Ports exposing (Ports)
 import Router as Router
 import Models.IncomingMessage as InMsg
-import Models.Game as Game
-import Models.NewRoom as NewRoom
-import Models.Result as Result
 import Json.Encode as JE
+import Update.NewRoom
+import Update.Game
 
 
 cmdOnRouteChange :
@@ -49,186 +46,6 @@ cmdOnRouteChange spec ports route prevRoute =
                 |> Maybe.withDefault Cmd.none
 
 
-updateRoomCmd :
-    Spec problem guess
-    -> Ports (Messages.Msg problem guess)
-    -> Game.Game problem guess
-    -> Cmd (Messages.Msg problem guess)
-updateRoomCmd spec ports model =
-    model.room
-        |> Maybe.map (Commands.UpdateRoom >> (Commands.commandEncoder spec.problemEncoder spec.guessEncoder) >> JE.encode 0 >> ports.outgoing)
-        |> Maybe.withDefault Cmd.none
-
-
-updateGame :
-    Spec problem guess
-    -> Ports (Messages.Msg problem guess)
-    -> GameMsg problem guess
-    -> Game.Game problem guess
-    -> ( Game.Game problem guess, Cmd (Messages.Msg problem guess) )
-updateGame spec ports msg model =
-    case msg of
-        ReceiveUpdate room ->
-            let
-                result =
-                    Result.get spec room
-
-                newProblemCmd =
-                    if (room.host == model.playerId) then
-                        (Random.generate (\pb -> Messages.GameMsg (ReceiveNewProblem pb)) spec.problemGenerator)
-                    else
-                        Cmd.none
-
-                ( newRoom, cmd ) =
-                    case result of
-                        Result.Pending ->
-                            ( room
-                            , if room.round.problem == Nothing then
-                                newProblemCmd
-                              else
-                                Cmd.none
-                            )
-
-                        Result.Winner winnerId ->
-                            ( if room.host == model.playerId then
-                                Room.setNewRound (Just winnerId) room
-                              else
-                                room
-                            , newProblemCmd
-                            )
-
-                        Result.Tie ->
-                            ( if room.host == model.playerId then
-                                Room.setNewRound Nothing room
-                              else
-                                room
-                            , newProblemCmd
-                            )
-            in
-                ( { model
-                    | room =
-                        Just newRoom
-                  }
-                , cmd
-                )
-
-        ReceiveNewProblem problem ->
-            let
-                newRoom =
-                    model.room
-                        |> Maybe.map
-                            (\rm ->
-                                let
-                                    oldRound =
-                                        rm.round
-
-                                    newRound =
-                                        { oldRound | problem = Just problem }
-                                in
-                                    { rm | round = newRound }
-                            )
-
-                newModel =
-                    { model | room = newRoom }
-
-                cmd =
-                    updateRoomCmd spec ports newModel
-            in
-                ( newModel
-                , cmd
-                )
-
-        Guess guess ->
-            let
-                newGuess =
-                    Just { value = guess, madeAt = model.ticksSinceNewRound }
-
-                player =
-                    model.room
-                        |> Maybe.map .players
-                        |> Maybe.andThen (Dict.get model.playerId)
-                        |> Maybe.map (\pl -> { pl | guess = newGuess })
-
-                newModel =
-                    { model
-                        | room =
-                            model.room
-                                |> Maybe.map2
-                                    (\player room ->
-                                        { room
-                                            | players = Dict.insert model.playerId player room.players
-                                        }
-                                    )
-                                    player
-                    }
-
-                cmd =
-                    updateRoomCmd spec ports newModel
-            in
-                ( newModel
-                , cmd
-                )
-
-        MarkReady ->
-            let
-                newRoom =
-                    model.room
-                        |> Maybe.map
-                            (Room.updatePlayer (\pl -> { pl | isReady = True }) model.playerId)
-
-                newModel =
-                    { model | room = newRoom }
-
-                cmd =
-                    updateRoomCmd spec ports newModel
-            in
-                ( newModel
-                , cmd
-                )
-
-        Tick time ->
-            ( { model
-                | ticksSinceNewRound =
-                    model.ticksSinceNewRound + 1
-              }
-            , Cmd.none
-            )
-
-
-updateNewRoom : NewRoomMsg -> NewRoom.NewRoom -> ( NewRoom.NewRoom, Bool )
-updateNewRoom msg model =
-    case msg of
-        ChangeRoomId newRoomId ->
-            ( { model | roomId = newRoomId }, False )
-
-        ChangePlayerId index value ->
-            ( { model
-                | playerIds =
-                    List.indexedMap
-                        (\index_ oldValue ->
-                            if index_ == index then
-                                value
-                            else
-                                oldValue
-                        )
-                        model.playerIds
-              }
-            , False
-            )
-
-        AddPlayer ->
-            ( { model | playerIds = model.playerIds ++ [ "" ] }, False )
-
-        RemovePlayer index ->
-            ( { model | playerIds = (List.take index model.playerIds) ++ (List.drop (index + 1) model.playerIds) }, False )
-
-        CreateRequest ->
-            ( { model | status = NewRoom.Pending }, True )
-
-        CreateResponse response ->
-            ( { model | status = NewRoom.Success }, False )
-
-
 update :
     Spec problem guess
     -> Ports (Msg problem guess)
@@ -248,7 +65,7 @@ update spec ports msg model =
         ( Router.Game game, GameMsg gameMsg ) ->
             let
                 ( newGame, cmd ) =
-                    updateGame spec ports gameMsg game
+                    Update.Game.update spec ports gameMsg game
             in
                 ( { model | route = Router.Game newGame }
                 , cmd
@@ -257,7 +74,7 @@ update spec ports msg model =
         ( Router.NewRoom newRoom, NewRoomMsg newRoomMsg ) ->
             let
                 ( newNewRoom, sendSaveCommand ) =
-                    (updateNewRoom newRoomMsg newRoom)
+                    (Update.NewRoom.update newRoomMsg newRoom)
             in
                 ( { model | route = Router.NewRoom newNewRoom }
                 , if sendSaveCommand then
@@ -273,7 +90,7 @@ update spec ports msg model =
         ( Router.NewRoom newRoom, IncomingSubscription (InMsg.RoomCreated room) ) ->
             ( { model
                 | route =
-                    updateNewRoom (CreateResponse "") newRoom
+                    Update.NewRoom.update (CreateResponse "") newRoom
                         |> Tuple.first
                         |> Router.NewRoom
               }
@@ -283,7 +100,7 @@ update spec ports msg model =
         ( Router.Game game, IncomingSubscription (InMsg.RoomUpdated room) ) ->
             let
                 ( newGame, cmd ) =
-                    updateGame spec ports (ReceiveUpdate room) game
+                    Update.Game.update spec ports (ReceiveUpdate room) game
             in
                 ( { model | route = Router.Game newGame }, cmd )
 
---
+++ src/Update/Game.elm
@@ -0,0 +1,158 @@
+module Update.Game exposing (..)
+
+import Random
+import Dict
+import Commands as Commands
+import Messages exposing (..)
+import Models.Room as Room
+import Gameroom.Spec exposing (Spec)
+import Models.Ports exposing (Ports)
+import Models.Game as Game
+import Models.Result as Result
+import Json.Encode as JE
+
+
+updateRoomCmd :
+    Spec problem guess
+    -> Ports (Messages.Msg problem guess)
+    -> Game.Game problem guess
+    -> Cmd (Messages.Msg problem guess)
+updateRoomCmd spec ports model =
+    model.room
+        |> Maybe.map (Commands.UpdateRoom >> (Commands.commandEncoder spec.problemEncoder spec.guessEncoder) >> JE.encode 0 >> ports.outgoing)
+        |> Maybe.withDefault Cmd.none
+
+
+update :
+    Spec problem guess
+    -> Ports (Messages.Msg problem guess)
+    -> GameMsg problem guess
+    -> Game.Game problem guess
+    -> ( Game.Game problem guess, Cmd (Messages.Msg problem guess) )
+update spec ports msg model =
+    case msg of
+        ReceiveUpdate room ->
+            let
+                result =
+                    Result.get spec room
+
+                newProblemCmd =
+                    if (room.host == model.playerId) then
+                        (Random.generate (\pb -> Messages.GameMsg (ReceiveNewProblem pb)) spec.problemGenerator)
+                    else
+                        Cmd.none
+
+                ( newRoom, cmd ) =
+                    case result of
+                        Result.Pending ->
+                            ( room
+                            , if room.round.problem == Nothing then
+                                newProblemCmd
+                              else
+                                Cmd.none
+                            )
+
+                        Result.Winner winnerId ->
+                            ( if room.host == model.playerId then
+                                Room.setNewRound (Just winnerId) room
+                              else
+                                room
+                            , newProblemCmd
+                            )
+
+                        Result.Tie ->
+                            ( if room.host == model.playerId then
+                                Room.setNewRound Nothing room
+                              else
+                                room
+                            , newProblemCmd
+                            )
+            in
+                ( { model
+                    | room =
+                        Just newRoom
+                  }
+                , cmd
+                )
+
+        ReceiveNewProblem problem ->
+            let
+                newRoom =
+                    model.room
+                        |> Maybe.map
+                            (\rm ->
+                                let
+                                    oldRound =
+                                        rm.round
+
+                                    newRound =
+                                        { oldRound | problem = Just problem }
+                                in
+                                    { rm | round = newRound }
+                            )
+
+                newModel =
+                    { model | room = newRoom }
+
+                cmd =
+                    updateRoomCmd spec ports newModel
+            in
+                ( newModel
+                , cmd
+                )
+
+        Guess guess ->
+            let
+                newGuess =
+                    Just { value = guess, madeAt = model.ticksSinceNewRound }
+
+                player =
+                    model.room
+                        |> Maybe.map .players
+                        |> Maybe.andThen (Dict.get model.playerId)
+                        |> Maybe.map (\pl -> { pl | guess = newGuess })
+
+                newModel =
+                    { model
+                        | room =
+                            model.room
+                                |> Maybe.map2
+                                    (\player room ->
+                                        { room
+                                            | players = Dict.insert model.playerId player room.players
+                                        }
+                                    )
+                                    player
+                    }
+
+                cmd =
+                    updateRoomCmd spec ports newModel
+            in
+                ( newModel
+                , cmd
+                )
+
+        MarkReady ->
+            let
+                newRoom =
+                    model.room
+                        |> Maybe.map
+                            (Room.updatePlayer (\pl -> { pl | isReady = True }) model.playerId)
+
+                newModel =
+                    { model | room = newRoom }
+
+                cmd =
+                    updateRoomCmd spec ports newModel
+            in
+                ( newModel
+                , cmd
+                )
+
+        Tick time ->
+            ( { model
+                | ticksSinceNewRound =
+                    model.ticksSinceNewRound + 1
+              }
+            , Cmd.none
+            )
