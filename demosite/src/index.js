4cee1d1e5f152c3a4bc97950ae7c066ff27f63d2
peterszerzo <szerzo.peter@gmail.com>
2017-05-25 15:40:55 +0200

    Streamline demo site

--- demosite/src/index.js
+++ demosite/src/index.js
@@ -1,5 +1,5 @@
 (function () {
-  var HASH = 'a1bcc54'
+  // This is filled out manually in the dist folder, right before uploading.
   var firebaseConfig = {
     apiKey: '',
     authDomain: '',
@@ -20,7 +20,7 @@
   })[0]
   if (game) {
     var scriptTag = document.createElement('script')
-    scriptTag.src = '/' + game + '.js?' + HASH
+    scriptTag.src = '/' + game + '.js?' + window.__ELM_GAMEROOM_HASH__
     scriptTag.onload = function () {
       var app = window.Elm.Main.embed(rootNode)
       window.talkToPorts(window.db(firebaseConfig, {rootRef: '/' + game}), app.ports)
@@ -30,12 +30,13 @@
     (function () {
       var html =
         '<div class="elm-gameroom-home">' +
-        '<h1>elm-gameroom demo</h1>' +
-        '<p>Hey, thanks for stopping by. Come play some games with your friends:</p>' +
+        '<svg><use xlink:href="#logo"></use></svg>' +
+        '<h1>Play elm-gamerooms</h1>' +
+        '<p>Hey, thanks for stopping by. Here are some playables for you and your friends:</p>' +
         games.map(function (game) {
           return '<a class="elm-gameroom-home-link" href="/' + game + '">' + game + '</a>'
         }).join('') +
-        '<p>This is a demo for <a href="http://package.elm-lang.org/packages/peterszerzo/elm-gameroom/latest">elm-gameroom</a></p>'
+        '<p>This is a demo for the <a class="elm-gameroom-home-simple-link" href="http://package.elm-lang.org/packages/peterszerzo/elm-gameroom/latest">elm-gameroom</a> project.</p>' +
         '</div>'
       rootNode.innerHTML = html
     }())
